b3545fa18eff72dc638640a0b4f184dc49f65443
Steven-ACP <127SCastroPacheco@frhsd.com>
2026-02-05 13:49:16 -0500

    fgfghghghghghghgh

--- JS/javascript.js
+++ JS/javascript.js
@@ -5,7 +5,7 @@ var apple = 1.25;
 var bread = 2.99;
 var milk = 3.49;
 var eggs = 4.25;
-
+var milkDiscount = disCountItem(milk, 0.1);
 var cartTotal = 0;
 
 function addToCart(item) {
@@ -14,11 +14,38 @@ function addToCart(item) {
 }
 
 function removeFromCartTotal(item) {
-  cartTotal = item - cartTotal;
+  cartTotal = cartTotal - item;
 }
 
+function disCountItem(item, discount) {
+  return item - item * discount;
+}
+
+function calcTotal() {
+  console.log(cartTotal.toFixed(2));
+}
+
+addToCart(banana);
+
+addToCart(banana);
+
+addToCart(banana);
+
+addToCart(banana);
+
+addToCart(banana);
+
 addToCart(apple);
-addToCart(milk);
-console.log(cartTotal);
-removeFromCartTotal(apple);
-console.log(cartTotal);
+
+addToCart(apple);
+
+addToCart(bread);
+
+addToCart(eggs);
+
+removeFromCartTotal(banana);
+
+removeFromCartTotal(banana);
+
+addToCart(milkDiscount);
+calcTotal();
